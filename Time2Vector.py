1fe76d65186484a155a43d6fdfde8315bd79ff49
cmajorsolo <cmajorsolo@gmail.com>
2021-09-05 16:33:39 +0100

    inital check in

---
+++ Time2Vector.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+import logging
+
+class Time2Vector(Layer):
+    def __init__(self, seq_len, **kwargs):
+        super().__init__()
+        self.seq_len = seq_len
+    
+    def build(self, input_shape):
+        logging.info('input_shape is {}'.format(input_shape))
+        self.weights_linear = self.add_weight(name='weight_linear',shape=(int(self.seq_len),),initializer='uniform',trainable=True)
+        self.bias_linear = self.add_weight(name='bias_linear',shape=(int(self.seq_len),),initializer='uniform',trainable=True)
+        self.weights_periodic = self.add_weight(name='weight_periodic', shape=(int(self.seq_len),), initializer='uniform', trainable=True)
+        self.bias_periodic = self.add_weight(name='bias_periodic', shape=(int(self.seq_len),), initializer='uniform', trainable=True)
+    
+    def call(self, x):
+        logging.info('inside call function')
+        logging.info('input x shape is {}'.format(x))
+        '''Calculate linear and periodic time features'''
+        x = tf.math.reduce_mean(x[:,:,:4], axis=-1) 
+        time_linear = self.weights_linear * x + self.bias_linear # Linear time feature
+        time_linear = tf.expand_dims(time_linear, axis=-1) # Add dimension (batch, seq_len, 1)
+
+        time_periodic = tf.math.sin(tf.multiply(x, self.weights_periodic) + self.bias_periodic)
+        time_periodic = tf.expand_dims(time_periodic, axis=-1) # Add dimension (batch, seq_len, 1)
+        return tf.concat([time_linear, time_periodic], axis=-1) # shape = (batch, seq_len, 2)
+    
+    def get_config(self):
+        # Needed for saving and loading model with custom layer
+        config = super().get_config().copy()
+        config.update({'seq_len': self.seq_len})
+        return config
